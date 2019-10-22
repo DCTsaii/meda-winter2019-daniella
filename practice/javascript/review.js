c600a880ab8012ec33094fdc37eeafddfe70b33a
Your Name <jobsdantsai@gmail.com>
2019-10-21 22:24:20 -0700

    node practice

--- practice/javascript/review.js
+++ practice/javascript/review.js
@@ -65,3 +65,5 @@ false;
 //Truthy
 // Everything else.
 
+
+
--- practice/node/createFile.js
+++ practice/node/createFile.js
@@ -1,17 +1,18 @@
 var fs = require("fs");
 
 // Write files, will overwrite existing filenames.
-fs.writeFileSync("my first file.txt", "I was created by a JavaScript Project", "utf8");
+fs.writeFileSync("my first file.txt", "I just replaced the old file", "utf8");
 
-fs.writeFileSync("mySecondFile.txt", "I created a second file using node", "utf8");
+fs.writeFileSync("this is another file.jpg", "Some other text", "utf8")
 
 // Read files, must load as utf8 to make sense.
-var textThatwasReadFromFile = fs.readFileSync("mySecondFile.txt", "utf8");
+var textThatWasReadFromFile = fs.readFileSync("some-file.txt", "utf8");
 
-
-// Update files, add to the end of the file.
+// Update files, adds to the end of the file.
 fs.appendFileSync("some-file.txt", 
-" This text file was updated", "utf8");
+" This text file was updated.", "utf8");
 
-console.log(textThatwasReadFromFile);
+
+
+console.log(textThatWasReadFromFile);
 
