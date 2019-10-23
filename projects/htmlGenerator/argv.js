fc1800fab1c115c7bfec66283e5f38cd5e61d1f5
student <student@PATC04.local>
2019-10-23 12:44:03 -0700

    argv.js

---
+++ projects/htmlGenerator/argv.js
@@ -0,0 +1,8 @@
+// process.argv only exist in Node.
+var terminalText = process.argv;
+
+console.log(terminalText);
+
+console.log("custome text only:");
+console.log(terminalText[2]);
+console.log(terminalText[3]);
