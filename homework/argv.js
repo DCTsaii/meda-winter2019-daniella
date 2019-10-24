7dee28389c61e2279643bd49f8caf38098d2b767
student <student@PATC04.local>
2019-10-24 08:57:14 -0700

    added homework in the homework folder

---
+++ homework/argv.js
@@ -0,0 +1,9 @@
+// process.argv only exist in Node.
+var terminalText = process.argv;
+
+console.log(terminalText);
+
+console.log("custome text only:");
+console.log(terminalText[2]);
+console.log(terminalText[3]);
+console.log(terminalText[4]);
