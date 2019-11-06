20f8f7d3e2136fca2db39b1cb626279977d1b737
student <student@PATC04.local>
2019-11-06 08:24:12 -0800

    truck homework

--- homework/vehicles/index.js
+++ homework/vehicles/index.js
@@ -23,8 +23,8 @@ honda.refuel(10); // filled with 10 gallons
 honda.travel(50); // travel 50 miles after refilling
 
 // Truck Homework
-let volvo = new Truck("Kevin", "Volvo", "VNR 640", "2019", "black", 21, 10, 21, 60, "diesel", 60);
+let volvo = new Truck("Kevin", "Volvo", "VNR 640", "2019", "black", 21, 10, 21, 60, "diesel", 60, 1000);
 
 console.log(volvo);
 volvo.transport(21);
-volvo.transport(343);
+volvo.transport(34388978);
--- homework/vehicles/truck.js
+++ homework/vehicles/truck.js
@@ -2,7 +2,7 @@ const Vehicle = require("./vehicle.js");
 
 const Truck = class Truck extends Vehicle {
   // the orders inside the constructor parameters must be the SAME order in the index.js file
-  constructor(owner, make, model, year, color, license, mileage, mpg, tankSize, engineType, topSpeed) {
+  constructor(owner, make, model, year, color, license, mileage, mpg, tankSize, engineType, topSpeed, capacity) {
       // super refers to constructor of the parent of the class
       super(make, model, year, color, owner, engineType, topSpeed); // super has to match position as the class in vehicle.js
       this.license = license;
@@ -10,15 +10,16 @@ const Truck = class Truck extends Vehicle {
       this.tankSize = tankSize;
       this.currentFuel = this.tankSize;
       this.MPG = mpg; // miles per gallon
+      this.capacity = capacity
   }
 
   transport(tons) {
     if (this.capacity < tons) { 
       console.log("No, this " + this.make + " " + this.model + " cannot handle " + tons + " tons.");
-    } else {
-      console.log("Yes, this " + this.make + " " + this.model + " can handle " + tons +   "tons.");
+    } else if (tons >= this.capacity){
+      console.log("Yes, this " + this.make + " " + this.model + " can handle " + tons +   " tons.");
     }
-    return  this.capacity >= tons; 
+     
   } 
 
 
