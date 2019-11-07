9f9c7ac8f3150e2135f0b7710bfcaf7e85f89045
student <student@PATC04.local>
2019-11-07 10:20:37 -0800

    Asked a second question for data set

--- practice/oop/vehicles/answers.js
+++ practice/oop/vehicles/answers.js
@@ -18,6 +18,9 @@ data = JSON.parse(data);
 // What is the average miles per gallon of these 100 cars.
 // To find out the average, we add up all the MPG properties and divide by how many cars there were.
 
+// Question 1 What is the average miles per gallon of these 100 cars?
+// Solution
+
 let total = 0;
 
 // .length is a property value for the array
@@ -30,3 +33,43 @@ let mpgAverage = total / data.cars.length;
 
 console.log("What is the average miles per gallon of these 100 cars?");
 console.log(mpgAverage + " MPG");
+
+// Question 1 END
+
+// QUESTION 2 START How many cars do I have that are built in between 1940 - 1960?
+
+// Solution 
+
+console.log("How many cars do we have that were built between 1940 and 1960, inclusive?");
+
+let cars = data.cars;
+// Step 1
+// Check the year of the car [the first car index]
+// console.log(cars[0].year); // This console.log to check if the .year is working in the terminal.
+
+// Step 3
+let carsBetween1940And1960 = 0;
+
+// Step 2
+// this for loop is to check for ALL the objects (cars) inside the array
+for(let i = 0; i < cars.length; i++){
+    // console.log(cars[i]);
+
+    let currentCar = cars[i];
+    // console.log(currentCar); to get if the let currentCar = cars[i] work. 
+// Step    
+    // This is harder to read and understand 
+    // if(currentCar.year >= 1940 && currentCar.year <= 1960){}
+    
+    // This is easier to read and understand
+    if(currentCar.year >= 1940){
+        if(currentCar.year <= 1960) {
+            carsBetween1940And1960 = carsBetween1940And1960 + 1;
+        }
+    }
+}
+
+console.log(carsBetween1940And1960);
+
+// this console.log will get you the last index
+// console.log(cars[cars.length - 1].year);
