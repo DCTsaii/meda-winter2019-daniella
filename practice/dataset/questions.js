53f70346bc719033da35669b8b8589f9bc6e973d
student <student@PATC04.local>
2019-11-19 12:55:04 -0800

    semi complete

--- practice/dataset/questions.js
+++ practice/dataset/questions.js
@@ -1,54 +1,5 @@
 const fs = require("fs");
 
-let data = fs.readFileSync("2018entries.json", "utf8"); // Store fs.readFileSync into a variable/let otherwise it will run as a string.
-
-data = JSON.parse(data); // convert the variable/let data into JSON file
-
-dataObjects = new Array();
-
-for (let i = 0; i < data.length; i++){
-    dataObjects.push(stringToObject(data[i]));
-}
-
-// Question 1
-console.log("What month had the most evictions?");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class EvictionNotice {
     constructor(month, zip, ellisAct, condoConversion){
         this.month = month;
@@ -57,6 +8,102 @@ class EvictionNotice {
         this.condoConversion = condoConversion;
     }
 }
+
+let data = fs.readFileSync("2018entries.json", "utf8"); // Store fs.readFileSync into a variable/let otherwise it will run as a string.
+
+data = JSON.parse(data); // convert the variable/let data into JSON file
+
+dataObjects = new Array();
+
+dataObjects = [];
+
+for (let i = 0; i < data.data.length; i++){
+    dataObjects.push(stringToObject(data.data[i]));
+}
+
+// Question 1
+console.log("What month had the most evictions?");
+
+
+// index 0 is January, index 11 is December
+let months = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+for(let i = 0; i < dataObjects.length; i++){
+    // efficient but hard to understand -- > months[parseInt(dataObjects[i].month) - 1]++;
+    switch(dataObjects[i].month){
+        case "01":
+            months[0]++;
+            break; 
+        case "02":
+            months[1]++;
+            break;
+        case "03":
+            months[2]++;
+            break; 
+        case "04":
+            months[3]++;
+            break; 
+        case "05":
+            months[4]++;
+            break;
+        case "06":
+            months[5]++;
+            break; 
+        case "07":
+            months[6]++;
+            break;
+        case "08":
+            months[7]++;
+            break;
+        case "09":
+            months[8]++;
+            break; 
+        case "10":
+            months[9]++;
+            break;
+        case "11":
+            months[10]++;
+            break; 
+        case "12":
+            months[11]++;
+            break;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // stringToObject(data.data[1000]); // data.data = first is the data file, second is the key inside the data file.
 
