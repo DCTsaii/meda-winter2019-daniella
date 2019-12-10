466682f2e3f3e1917bd818fb27bdc70e05301911
student <student@PATC04.local>
2019-12-10 10:08:51 -0800

    React review

--- practice/react-practice/myfirstapp/src/index.js
+++ practice/react-practice/myfirstapp/src/index.js
@@ -1,11 +1,24 @@
 // import vs require
 
+/* Quick Review
+let myFirstReactElement = (
+    <h1 id="firstHeader">Hello World!</h1>
+);
+
+ReactDOM.render(
+    myFirstReactElement,
+    // document.querySelector("body") 
+    document.getElementById("root")
+);
+*/
+
 // Include React packages to use in program
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create our first component, barebones.
-// first argument is name of element, second argument is attributes, third argument is element.
+// first argument is name of element, second argument is attributes, third is content of element.
 const title = React.createElement(
     "h1",
     {
@@ -19,6 +32,13 @@ const paragraph = React.createElement(
     null,
     "This is a paragraph!"
 );
+
+const paragraph2 = React.createElement(
+    "p",
+    null,
+    "This is a second paragraph."
+);
+
 const idOfDiv = "test";
 // JSX version of createElements();
 const app = (
@@ -26,10 +46,19 @@ const app = (
     <div id={idOfDiv}>
         {title}
         {paragraph}
+        {paragraph2}
     </div>
 );
 // Render component to target element.
 ReactDOM.render(
-    app,
-    document.getElementById("root")
+    app, // item you want to render
+    document.getElementById("root") // where you want to render to
 );
+
+// JSX Syntax is XML
+// Anything that is written in JSX should go in between a pair of parentheses.
+// Any JavaScript inside of JSX should be written between a pair of curly braces.
+// EXAMPLE:
+// let text = (<p>{someJSVariable}</p>);
+
+// let text2 = <p></p>
